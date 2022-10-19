89f720e01d9ad7a45daab832c833b335cda021f1
ofishea <fishflowbean@gmail.com>
2022-10-19 23:35:24 +0100

    Add files via upload

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+import { DetailedPage } from './detailed/detailed.page';
+
+const routes: Routes = [
+  { path: '', component: HomePage, },
+  { path: 'detailed', component: DetailedPage, }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomePageRoutingModule {}
